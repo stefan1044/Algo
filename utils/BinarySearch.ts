f23d0428561744447c1487c6f88e051b02f27f0f
stefan1044 <stefan.harabagiu2000@gmail.com>
2024-05-26 10:51:45 +0300

    asd

--- utils/BinarySearch.ts
+++ utils/BinarySearch.ts
@@ -17,3 +17,26 @@ function binarySearch(array: Array<number>, searchValue: number): number{
 
     return -1;
 }
+
+function binarySearchClosest(array: Array<number>, searchValue: number): number {
+    let left = 0;
+    let right = array.length - 1;
+    let middle;
+
+    while (left != right) {
+        middle = Math.ceil((left + right) / 2);
+
+        if (array[middle] > searchValue)
+            right = middle - 1;
+        else
+            left = middle;
+    }
+
+    if (array[left] > searchValue)
+        return left - 1;
+    if (array[left] < searchValue)
+        return left;
+    if (array[left] == searchValue)
+        return left;
+}
+
